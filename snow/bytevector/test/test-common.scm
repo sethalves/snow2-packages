df03ca01bab544c7280c519e3cfd9b405c87fc5b
Seth Alves <seth@gloebit.com>
2014-03-16 10:37:56 -0700

    added reverse-bytevector-list->latin-1-string and reverse-bytevector-list->bytevector

--- snow/bytevector/test/test-common.scm
+++ snow/bytevector/test/test-common.scm
@@ -1,7 +1,50 @@
 
 (define (main-program)
-  (let ((bv (bytevector 1 2 3 4)))
-    (display bv)
-    (newline)
+  (and
 
-    bv))
+   (let ((bv (bytevector 1 2 3 4)))
+     (display bv)
+     (newline)
+     bv)
+
+   (equal?
+    (reverse-bytevector-list->latin-1-string
+     (list (string->latin-1 "def")
+           (string->latin-1 "abc")))
+    "abcdef")
+
+
+   (equal?
+    (reverse-bytevector-list->latin-1-string
+     (list (string->latin-1 "")
+           (string->latin-1 "abc")))
+    "abc")
+
+
+   (equal?
+    (reverse-bytevector-list->latin-1-string
+     (list (string->latin-1 "abc")
+           (string->latin-1 "")))
+    "abc")
+
+
+   (equal?
+    (reverse-bytevector-list->latin-1-string
+     (list (string->latin-1 "")
+           (string->latin-1 "")))
+    "")
+
+   (equal?
+    (reverse-bytevector-list->latin-1-string
+     (list))
+    "")
+
+
+   (equal?
+    (reverse-bytevector-list->bytevector
+     (list (bytevector 4 5 6)
+           (bytevector 1 2 3)))
+    (bytevector 1 2 3 4 5 6))
+
+
+   ))
