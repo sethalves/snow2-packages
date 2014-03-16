495ef19ff576b72d68070f6d20984f99475bacaa
Seth Alves <seth@gloebit.com>
2014-03-15 17:35:02 -0700

    handle binary ports

--- seth/mime/test/test-chicken.scm
+++ seth/mime/test/test-chicken.scm
@@ -6,7 +6,9 @@ exec csi -s $0 "$@"
 (use r7rs)
 (import-for-syntax r7rs)
 
+(include "snow/snowlib.sld")
 (include "snow/bytevector.sld")
+(include "snow/binio.sld")
 (include "snow/srfi-13-strings.sld")
 (include "snow/srfi-60-integers-as-bits.sld")
 (include "seth/quoted-printable.sld")
