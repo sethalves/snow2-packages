f0d857ff089f74171e6d0dbe5422aabab2626c08
Seth Alves <seth.alves@gmail.com>
2014-10-10 14:01:48 -0700

    fix up tests for processio

--- snow/snow/processio/test-gauche.scm
+++ snow/snow/processio/test-gauche.scm
@@ -8,13 +8,6 @@ exec gosh \
 
 (import (scheme base)
         (scheme write)
-        (scheme process-context)
-        (snow filesys)
-        (snow extio)
-        (snow assert)
-        (snow processio))
-
-(include "test-common.scm")
-
-(display (main-program))
+        (snow processio tests))
+(display (run-tests))
 (newline)
